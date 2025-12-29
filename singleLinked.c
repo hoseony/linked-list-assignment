35d0195bd83d25b03593633294cbbb32c79c7ead
hoseony <hoseon.alex.yu@gmail.com>
2025-12-28 22:23:16 -0800

    list alloc

--- singleLinked.c
+++ singleLinked.c
@@ -1,19 +1,46 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct node_s {
-int data;
-struct node_s *next;
+	int data;
+	struct node_s *next;
 } node_t;
+
 typedef struct list_s {
-node_t *head;
-node_t *tail;
-size_t size;
+	node_t *head;
+	node_t *tail;
+	size_t size;
 } list_t;
+
+
 // functions to implement
-list_t *list_alloc(void);
-void list_free(list_t *list);
-int list_prepend(list_t *list, int val);
-int list_append(list_t *list, int val);
-int list_insert(list_t *list, int val, size_t pos);
-int list_rm(list_t *list, int *val, size_t pos);
-int list_set(list_t *list, int val, size_t pos);
-int list_get(list_t *list, int *val, size_t pos);
+
+list_t *list_alloc(void) {
+	list_t *list;
+  list = NULL;
+
+  list = malloc(sizeof(list_t)); // dynamically allocate
+  
+  if (list == NULL) { //check if allocation fails
+    return NULL;
+  };
+
+  return list;
+}
+
+int main(void) {
+  list_t *list = list_alloc();
+
+  if (!list) {
+    printf("list_alloc failed");
+    return 1;
+  }
+
+  printf("pointer %p\n", (void *)list);
+  printf("list->head %p\n", (void *)list->head);
+  printf("list->tail %p\n", (void *)list->tail);
+  printf("list->size %zu\n", list->size);
+
+  return 0;
+}
