18b7376eafe7fa013ac27ef014b2c81f1962212c
hoseony <hoseon.alex.yu@gmail.com>
2025-12-30 23:39:25 -0800

    added list_free

--- singleLinked.c
+++ singleLinked.c
@@ -24,10 +24,12 @@ list_t *list_alloc(void) {
   
   if (list == NULL) { //check if allocation fails
     return NULL;
-  };
+  }
 
   return list;
 }
+
+/** test code for list_alloc()
 
 int main(void) {
   list_t *list = list_alloc();
@@ -46,3 +48,50 @@ int main(void) {
 
   return 0;
 }
+**/
+
+void list_free(list_t *list) {
+
+  if (list == NULL) {
+    return;
+  }
+
+  node_t *ptr;
+  ptr = list->head;
+
+  while (ptr != NULL) {
+    node_t *next = ptr->next;
+    free(ptr);
+    ptr = next;
+  }
+
+  free(list);
+}
+
+
+//test code for list_free with dummy data
+int main(void) {
+  list_t *list = list_alloc();
+
+  node_t *a = malloc(sizeof(node_t));
+  node_t *b = malloc(sizeof(node_t));
+  node_t *c = malloc(sizeof(node_t));
+
+  a->data = 10;
+  b->data = 20;
+  c->data = 30;
+
+  a->next = b;
+  b->next = c;
+  c->next = NULL;
+
+  list->head = a;
+  list->tail = c;
+  list->size = 3;
+
+  printf("pointer %p\n", (void *)list->head);
+  printf("pointer %p\n", (void *)list->tail);
+  printf("pointer %p\n", (void *)list->size);
+
+  list_free(list);
+} //tested with leaks --atExist -- ./singleLinked
