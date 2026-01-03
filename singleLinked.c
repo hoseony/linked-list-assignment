0ebfa82ddef5c9b7f5980f5232e7f2664907fdc7
hoseony <hoseon.alex.yu@gmail.com>
2026-01-02 16:59:50 -0800

    added list_insert, removed some testing codes

--- singleLinked.c
+++ singleLinked.c
@@ -14,7 +14,6 @@ typedef struct list_s {
 } list_t;
 
 
-// functions to implement
 
 list_t *list_alloc(void) {
 	list_t *list;
@@ -29,26 +28,6 @@ list_t *list_alloc(void) {
   return list;
 }
 
-/** test code for list_alloc()
-
-int main(void) {
-  list_t *list = list_alloc();
-
-  if (!list) {
-    printf("list_alloc failed");
-    return 1;
-  }
-
-  printf("pointer %p\n", (void *)list);
-  printf("list->head %p\n", (void *)list->head);
-  printf("list->tail %p\n", (void *)list->tail);
-  printf("list->size %zu\n", list->size);
-  
-  free(list);
-
-  return 0;
-}
-**/
 
 void list_free(list_t *list) {
 
@@ -68,35 +47,6 @@ void list_free(list_t *list) {
   free(list);
 }
 
-/** test code fzor list_free with dummy data
-
-int main(void) {
-  list_t *list = list_alloc();
-
-  node_t *a = malloc(sizeof(node_t));
-  node_t *b = malloc(sizeof(node_t));
-  node_t *c = malloc(sizeof(node_t));
-
-  a->data = 10;
-  b->data = 20;
-  c->data = 30;
-
-  a->next = b;
-  b->next = c;
-  c->next = NULL;
-
-  list->head = a;
-  list->tail = c;
-  list->size = 3;
-
-  printf("pointer %p\n", (void *)list->head);
-  printf("pointer %p\n", (void *)list->tail);
-  printf("pointer %p\n", (void *)list->size);
-
-  list_free(list);
-} //tested with leaks --atExist -- ./singleLinked, 0 leaks
-
-**/
 
 int list_prepend(list_t *list, int val) {
 
@@ -120,6 +70,7 @@ int list_prepend(list_t *list, int val) {
   list->size++;
   return 0;
 }
+
 
 int list_append(list_t *list, int val) {
 
@@ -147,9 +98,6 @@ int list_append(list_t *list, int val) {
   return 0;
 }
 
-
-
-
 //made for debugging purpose
 void list_print(list_t *list) {
   if (list == NULL) {
@@ -171,28 +119,43 @@ void list_print(list_t *list) {
 }
 
 
-/**
+int list_insert(list_t *list, int val, size_t pos) {
+  if (list == NULL) {
+    return 1;
+  }
 
-//testing code for list_prepend
-int main(void) {
-  list_t *list = list_alloc();
+  if (pos == 0) {
+    return list_prepend(list, val);
+  } else if (pos == list->size) {
+    return list_append(list, val);
+  } else if (pos > list->size) {
+    return 1;
+  } else {
 
-  printf("pointer %p\n", (void *)list->head);
-  printf("pointer %p\n", (void *)list->tail);
-  printf("pointer %p\n", (void *)list->size);
+    node_t *prev = list->head;
 
-  list_prepend(list, 10);
+    for (size_t i = 0; i < pos - 1; i++) {
+      prev = prev->next;
+    }
 
-  printf("pointer %p\n", (void *)list->head);
-  printf("pointer %p\n", (void *)list->tail);
-  printf("pointer %p\n", (void *)list->size);
-  printf("data %d\n", list->head->data);
-  printf("next %p\n", (void *)list->head->next);
+    node_t *newNode = malloc(sizeof(node_t));
+    if (newNode == NULL) {
+      return 1;
+    }
 
-  list_free(list);
+    newNode->data = val;
+
+    newNode->next = prev->next;
+    prev->next = newNode;
+
+    list->size++;
+    return 0;
+  }
+
 }
 
-**/
+
+
 
 int main(void) {
   list_t *list = list_alloc();
@@ -200,7 +163,10 @@ int main(void) {
   list_prepend(list, 1);
   list_prepend(list, 2);
   list_prepend(list, 3);
-  list_append(list, 4);
+  list_append(list, 0);
+
+  list_print(list);
+  list_insert(list, 30, 3);
 
   list_print(list);
   list_free(list);
